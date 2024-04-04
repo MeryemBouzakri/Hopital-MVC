eeadf3dbc0d65bce40c065f0c26e3404c7392ea4
meryem3005 <bouzekrimeryem@gmail.com>
2024-04-04 13:31:59 +0000

    first commit

--- src/main/java/com/example/hospital2/entities/Patient.java
+++ src/main/java/com/example/hospital2/entities/Patient.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 @Entity
@@ -14,6 +15,8 @@ public class Patient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nom;
+    @DateTimeFormat(pattern = "yyy-MM-dd")
+
     private Date dateNaissance;
     private boolean malade;
     private int score ;
--- src/main/java/com/example/hospital2/web/PatientController.java
+++ src/main/java/com/example/hospital2/web/PatientController.java
@@ -2,13 +2,16 @@ package com.example.hospital2.web;
 
 import com.example.hospital2.entities.Patient;
 import com.example.hospital2.repository.PatientRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -37,4 +40,25 @@ public class PatientController {
         patientRepository.deleteById(id);
         return "redirect:/index?"+page+"$keyword="+keyword;
     }
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+    @GetMapping("/formPatients")
+    public String formPatient(Model model) {
+        model.addAttribute("patient", new Patient()); // Ajoute un patient vide pour le binding du formulaire
+        return "formPatients";
+    }
+    @PostMapping("/save")
+    public String save( Model model , @Valid Patient patient , BindingResult bindingResult, @RequestParam(defaultValue = "") String keyword , @RequestParam(defaultValue = "0") int page){
+        if (bindingResult.hasErrors()) return "formPatients";
+        patientRepository.save(patient);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+
+
+
+
+
 }
